eea5eda4868d17f5041c5f88662bc5ed17531b0d
amfi-disable <evengao6688@gmail.com>
2026-07-19 21:56:06 +0800

    chore: bump juicer cask to 1.0.1

--- casks/juicer.rb
+++ casks/juicer.rb
@@ -1,8 +1,8 @@
 cask "juicer" do
-  version "1.0.0"
-  sha256 "8d17728918f039f1a0e41312c2834a37faf8348f9d408358e2815c17296bab21"
+  version "1.0.1"
+  sha256 "da29c78f13fe38e38e2410d9474a658c303a80b0d64842466497eaef5d99ef43"
 
-  url "https://github.com/amfi-disable/Juicer/releases/download/V1.0.0/Juicer.zip"
+  url "https://github.com/amfi-disable/Juicer/releases/download/V1.0.1/juicer.zip"
   name "Juicer"
   desc "Premium native macOS developer suite"
   homepage "https://github.com/amfi-disable/Juicer"
