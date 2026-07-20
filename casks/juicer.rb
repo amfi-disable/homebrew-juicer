071c99ad4b7e2badc16f53ca9643965188aaf2d9
amfi-disable <evengao6688@gmail.com>
2026-07-20 09:30:50 +0800

    fix(cask): update sha256 checksum for V1.0.1-Beta icon fix

--- casks/juicer.rb
+++ casks/juicer.rb
@@ -1,6 +1,6 @@
 cask "juicer" do
   version "1.0.1"
-  sha256 "deb824f4d73d60ff783eb85787f4406cb2447c01b16f44358dc6a342031a5107"
+  sha256 "b1c45d99b54354138ef9c372655101412f422af8c3fd4451123b91d666234ea2"
 
   url "https://github.com/amfi-disable/Juicer/releases/download/V1.0.1-Beta/juicer.zip"
   name "Juicer"
