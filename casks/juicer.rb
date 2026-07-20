aa09687501018e73881c78b254561ff0e3b7dd44
amfi-disable <evengao6688@gmail.com>
2026-07-20 09:27:17 +0800

    fix(cask): point download url to V1.0.1-Beta release

--- casks/juicer.rb
+++ casks/juicer.rb
@@ -2,7 +2,7 @@ cask "juicer" do
   version "1.0.1"
   sha256 "deb824f4d73d60ff783eb85787f4406cb2447c01b16f44358dc6a342031a5107"
 
-  url "https://github.com/amfi-disable/Juicer/releases/download/V1.0.1/juicer.zip"
+  url "https://github.com/amfi-disable/Juicer/releases/download/V1.0.1-Beta/juicer.zip"
   name "Juicer"
   desc "Premium native macOS developer suite"
   homepage "https://github.com/amfi-disable/Juicer"
