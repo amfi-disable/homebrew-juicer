014850df6d31f33cc8bac11a824c71f0635d7871
amfi-disable <evengao6688@gmail.com>
2026-07-21 10:47:54 +0800

    fix(cask): update SHA256 for app icon rendering fix

--- casks/juicer.rb
+++ casks/juicer.rb
@@ -1,6 +1,6 @@
 cask "juicer" do
   version "1.0.1"
-  sha256 "c27771f04837746ac53ff132cb5f1f96c382accf8bea81290ec0ac07c058d851"
+  sha256 "541de542d958f3685b3e8cb66c1045c83dcdf53b4ad8a56bc595f22d09bda4a4"
 
   url "https://github.com/amfi-disable/Juicer/releases/download/V1.0.1/juicer.zip"
   name "Juicer"
